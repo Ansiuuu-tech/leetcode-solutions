47ce3c76b79477c1633b471e37cb38074bed5470
Anshu Gaur <anshugaur718@gmail.com>
2026-05-18 21:18:55 +0530

    Time: 2 ms (72.68%), Space: 44.9 MB (34.13%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    void solve(List<String> ans, int open, int close, String curr, int n) {
+
+        if(curr.length() == 2 * n) {
+            ans.add(curr);
+            return;
+        }
+
+        if(open < n) {
+            solve(ans, open + 1, close, curr + "(", n);
+        }
+
+        if(close < open) {
+            solve(ans, open, close + 1, curr + ")", n);
+        }
+    }
+
+    public List<String> generateParenthesis(int n) {
+
+        List<String> ans = new ArrayList<>();
+
+        solve(ans, 0, 0, "", n);
+
+        return ans;
+    }
+}
